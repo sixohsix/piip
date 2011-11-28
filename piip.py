fca450361aecd49eb0335402ee706e4c0a36988c
Mike Verdone <mike.verdone@gmail.com>
2011-11-29 00:23:59 +0100

    Added exception throwing and handling. Don't look too close, you'll go blind.

--- piip.py
+++ piip.py
@@ -1,20 +1,12 @@
 
-class NoSuchAttribute(Exception):
+class _NoSuchAttribute(AttributeError):
     def __init__(self, k):
         self.k = k
 
 
 class Bottom(object):
     def _p_get_rec(self, k):
-        raise NoSuchAttribute(k)
-
-
-def late_bind(func):
-    def _p_late_binding(self):
-        def _p_late_bound(*args, **kwargs):
-            return func(*[self] + list(args), **kwargs)
-        return _p_late_bound
-    return _p_late_binding
+        raise _NoSuchAttribute(k)
 
 
 class PObjectBase(object):
@@ -38,6 +30,22 @@ class PObjectBase(object):
         else:
             return o
 
+    def __str__(self):
+        return self.show()
+
+
+class PErrorBase(PObjectBase, BaseException):
+    __init__ = PObjectBase.__init__
+
+
+def late_bind(func):
+    def _p_late_binding(self):
+        def _p_late_bound(*args, **kwargs):
+            return func(*[self] + list(args), **kwargs)
+        _p_late_bound.__doc__ = getattr(func, '__doc__', None)
+        return _p_late_bound
+    return _p_late_binding
+
 
 pobject = PObjectBase(Bottom())
 
@@ -52,7 +60,35 @@ def new(self):
     return PObjectBase(self)
 
 
+@pobject.bind
+def show(self):
+    attrs = pattributes(self)
+    return "<@ %s >" % str(attrs)
+
+
 def pattributes(o):
     d = dict(o.__dict__)
     d.pop('prototype', None)
     return d
+
+
+def failure(o):
+    if not pattributes(o).get('_e_typ'):
+        class _Failure(PErrorBase):
+            pass
+        o._e_typ = _Failure
+    return o._e_typ
+
+
+perror = PErrorBase(pobject)
+
+@perror.bind
+def throw(self, **kwargs):
+    o = self.new()
+    for k,v in kwargs.items():
+        setattr(o, k, v)
+    raise failure(self)(o)
+
+
+no_such_attribute = perror.new()
+no_such_attribute._e_typ = _NoSuchAttribute
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 
-from piip import pobject, pattributes
+from piip import pobject, pattributes, perror, no_such_attribute, failure
 
 
 def test_can_extend_pobj():
@@ -63,3 +63,45 @@ def test_can_assign_my_prototype():
     my_o2.prototype = other_o
     assert 9 == my_o2.asdf
 
+
+def test_custom_str_format():
+    my_o = pobject.new()
+
+    @my_o.bind
+    def show(self):
+        return "hello!"
+
+    assert "hello!" == str(my_o)
+
+
+def test_accessing_no_attr_raises_no_such_attribute():
+    try:
+        pobject.flummox
+        assert False
+    except failure(no_such_attribute):
+        pass
+
+
+def test_can_raise_and_catch_piip_errors():
+    my_err = perror.new()
+    ok = False
+    try:
+        my_err.throw()
+        assert False
+    except failure(my_err), e:
+        ok = True
+    except:
+        assert False
+
+
+def test_can_raise_and_catch_piip_errors_with_kwarg():
+    my_err = perror.new()
+    ok = False
+    try:
+        my_err.throw(kwarg='kwarg')
+        assert False
+    except failure(my_err), e:
+        assert e.kwarg == 'kwarg'
+        ok = True
+    except:
+        assert False
