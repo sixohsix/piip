7fac4e346940b29a9df2fe14cb58776cdab09e98
Mike Verdone <mike.verdone@gmail.com>
2011-11-28 22:43:50 +0100

    This is so fun.

--- piip.py
+++ piip.py
@@ -4,22 +4,6 @@ class NoSuchAttribute(Exception):
         self.k = k
 
 
-class pdict(dict):
-    def __init__(self, proto_dict):
-        self.__pdict = proto_dict
-
-    def __getitem__(self, k):
-        try:
-            return dict.__getitem__(self, k)
-        except KeyError:
-            return self.__pdict[k]
-
-
-class root_pdict(dict):
-    def __getitem__(self, k):
-        raise NoSuchAttribute(k)
-
-
 class Bottom(object):
     def _p_get_rec(self, k):
         raise NoSuchAttribute(k)
@@ -35,30 +19,40 @@ def late_bind(func):
 
 class PObjectBase(object):
     def __init__(self, prototype):
-        self._prototype = prototype
+        self.prototype = prototype
 
     def _p_get_rec(self, k):
         try:
             o = object.__getattribute__(self, k)
         except AttributeError:
-            o = self._prototype._p_get_rec(k)
+            o = self.prototype._p_get_rec(k)
         return o
-
-    def new(self):
-        return PObjectBase(self)
 
     def __getattribute__(self, k):
         try:
             o = object.__getattribute__(self, k)
         except AttributeError:
-            o = self._prototype._p_get_rec(k)
+            o = self.prototype._p_get_rec(k)
         if hasattr(o, '__name__') and o.__name__ == '_p_late_binding':
             return o(self)
         else:
             return o
 
-    def bind(self, func):
-        setattr(self, func.__name__, late_bind(func))
-
 
 pobject = PObjectBase(Bottom())
+
+def bind(self, func):
+    setattr(self, func.__name__, late_bind(func))
+
+pobject.bind = late_bind(bind)
+
+
+@pobject.bind
+def new(self):
+    return PObjectBase(self)
+
+
+def pattributes(o):
+    d = dict(o.__dict__)
+    d.pop('prototype', None)
+    return d
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 
-from piip import pobject
+from piip import pobject, pattributes
 
 
 def test_can_extend_pobj():
     my_o = pobject.new()
     assert my_o is not pobject
+
 
 def test_can_read_val_of_extended_pobj():
     my_o = pobject.new()
@@ -36,3 +37,29 @@ def test_bound_methods_are_bound_late_enough():
 
     assert 11 == my_o.my_method(7)
     assert 13 == my_o2.my_method(7)
+
+
+def test_can_get_only_my_attributes():
+    my_o = pobject.new()
+    my_o.asdf = 4
+    my_o2 = my_o.new()
+    my_o2.vrrf = 6
+
+    assert pattributes(my_o2) == dict(vrrf=6)
+
+
+def test_can_get_my_prototype():
+    my_o = pobject.new()
+    my_o2 = my_o.new()
+    assert my_o2.prototype is my_o
+
+
+def test_can_assign_my_prototype():
+    my_o = pobject.new()
+    my_o.asdf = 7
+    other_o = pobject.new()
+    other_o.asdf = 9
+    my_o2 = my_o.new()
+    my_o2.prototype = other_o
+    assert 9 == my_o2.asdf
+
